cd0c6c27a5cfb3e3d11ac70981778f2fcc20f3fe
rohanbadgujar20011 <rohanbadgujar2001@gmail.com>
2025-12-03 19:48:10 +0530

    Added Eureka server

---
+++ ecom-eureka-server/src/main/java/com/ecom/eureka/server/EcomEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomEurekaServerApplication.class, args);
+	}
+
+}
---
+++ ecom-eureka-server/src/test/java/com/ecom/eureka/server/EcomEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
