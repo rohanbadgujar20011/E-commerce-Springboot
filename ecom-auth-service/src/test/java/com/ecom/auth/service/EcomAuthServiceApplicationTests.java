9a1eaffd111fcb33b8fa4d76eecbabbc9d604bb9
rohanbadgujar20011 <rohanbadgujar2001@gmail.com>
2025-12-03 20:11:11 +0530

    Added Structure

---
+++ ecom-auth-service/src/test/java/com/ecom/auth/service/EcomAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
